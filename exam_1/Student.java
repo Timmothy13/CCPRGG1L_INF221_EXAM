7d40ba84962c911fe491fef40b61357fb1a3d895
Jan-Timmothy Lukban <111472753+Timmothy13@users.noreply.github.com>
2022-09-05 10:21:17 +0800

    Add files via upload

---
+++ exam_1/Student.java
@@ -0,0 +1,28 @@
+public class Character {
+    
+    String surname;
+    String firstname;
+    char middleIntial;
+    int dateOfBirth;
+    int studentNumber;
+    String emailAddress;
+    boolean iAmAwesome;
+
+   
+    public void sayMyStudentNumber() {
+        System.out.println("Hello my Student Number is " + studentNumber);
+    }
+
+    public void sayMyEmailAddress() {
+        System.out.println("Hello my Email Address is " + emailAddress);
+    }
+
+    public void sayiAmAwesome() {
+        System.out.println("amIAwesome: " + iAmAwesome);
+    }
+
+
+    
+
+    
+    }
